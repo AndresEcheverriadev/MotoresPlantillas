69c8858eae39784db8e7496a6e938b51798c8bb8
Andres Echeverria <andres@brandpro.cl>
2022-06-21 15:55:21 -0400

    methods ok

--- Apirestful.js
+++ Apirestful.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app = express();
 const router = express.Router();
@@ -18,13 +18,13 @@ const productos = [
         title:"iMac",
         price : 6500,
         thumbnail: "https://cdn4.iconfinder.com/data/icons/apple-products-2026/512/iMac-512.png",
-        id: "2"
+        id: 2
     },
     {
         title:"Apple TV",
         price : 800,
         thumbnail: "https://cdn1.iconfinder.com/data/icons/apple-products-2026/512/Apple_TV__Ovladac-512.png",
-        id: "3"
+        id: 3
     },
     {
         title:"Ipad",
@@ -44,13 +44,22 @@ router.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 router.get('', (req,res) => {
-    res.json(productos);
+    if(productos.length == 0){
+        res.json('No hay productos');
+    }else {
+        res.json(productos);
+    };
 }
 ); 
 
 router.get('/:id', (req,res) => {
-    const productofiltrado = productos.filter(prod => prod.id === req.params.id);
-    res.json(productofiltrado);
+    const productofiltrado = productos.filter(prod => JSON.stringify(prod.id) === req.params.id);
+    if(productofiltrado.length == 0) {
+        res.json('Producto no existe');
+    }
+    else {
+        res.json(productofiltrado);
+    };
 }
 ); 
 
@@ -68,17 +77,28 @@ router.post('/guardar', (req,res) => {
 );
 
 router.put('/editar/:id', (req,res) => {
-    let productofiltrado = productos.filter(prod => prod.id === req.params.id);
-    
-    res.json({result:'ok',
-            id: req.params.id,
-            nuevo: req.body});
+    productos[(req.params.id)-1].title = req.body.title;
+    productos[(req.params.id)-1].price = req.body.price;
+    productos[(req.params.id)-1].thumbnail = req.body.thumbnail;
+    res.json({result: "Producto editado:",title: req.body.title,
+    price: req.body.price,
+    thumbnail: req.body.thumbnail,
+    id:  JSON.parse(req.params.id)});
+}
+);
+
+router.delete('/borrar/:id', (req,res) => {
+    productos.splice(req.params.id-1,1); 
+    res.json({result: "Producto borrado:",title: productos.title,
+    price: productos.price,
+    thumbnail: productos.thumbnail,
+    id:  JSON.parse(req.params.id)});
 }
 ); 
 
-// const nuevo = {
-//     title:'iPod Shuffle',
-//     price : 500,
-//     thumbnail: 'https://www.iconfinder.com/icons/2041366/apple_ipod_mp3_music_shuffle_icon',
-//     id: newID
-// };
+const nuevo = {
+    "title":'iPod Shuffle',
+    "price" : 500,
+    "thumbnail": 'https://www.iconfinder.com/icons/2041366/apple_ipod_mp3_music_shuffle_icon',
+    "id": 2
+};
