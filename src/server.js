f1a8edd71c47cbd5d7e653fb06c0dea95a00c850
Andres Echeverria <andres@brandpro.cl>
2022-06-27 16:28:33 -0400

    hbs ok

---
+++ src/server.js
@@ -0,0 +1,79 @@
+import express,{ Router } from 'express'
+import handlebars from "express-handlebars";
+import { Productos } from './Api.js'
+
+const app = express();
+const port = 8080;
+const routerProductos = Router();
+const productNotFound = 'Producto no encontrado';
+const apiProductos = new Productos();
+
+app.engine(
+    "hbs",
+    handlebars.engine({
+      extname: ".hbs",
+      defaultLayout: "main.hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", "../views");
+app.use(express.static('../public'));
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use('/productos', routerProductos);
+
+app.get("/", (req, res) => {
+    res.render('./form');
+});
+
+
+
+
+
+routerProductos.get("/", (req, res) => {
+    const response = apiProductos.getAll();
+  
+    if (!response) res.send({ error: productNotFound });
+  
+    res.render("productos", { productos: response });
+});
+  
+routerProductos.post("/", (req, res) => {
+    const { title, price, thumbnail } = req.body;
+  
+    apiProductos.save({ title, price, thumbnail });
+  
+    res.redirect("/");
+});
+
+
+const server = app.listen(port, () => {
+    console.log(`Servidor corriendo en puerto ${port}`)
+}); 
+server.on('error', error => console.log(`Error en servidor ${error}`))
+
+
+
+// toTestProductos   = 
+//     {
+//         title:"iPhoneX",
+//         price : 1500,
+//         thumbnail: "https://cdn4.iconfinder.com/data/icons/apple-products-2026/512/iPhone_X_home-screen-512.png",
+//     },
+//     {
+//         title:"iMac",
+//         price : 6500,
+//         thumbnail: "https://cdn4.iconfinder.com/data/icons/apple-products-2026/512/iMac-512.png",
+//     },
+//     {
+//         title:"Apple TV",
+//         price : 800,
+//         thumbnail: "https://cdn1.iconfinder.com/data/icons/apple-products-2026/512/Apple_TV__Ovladac-512.png",
+//     },
+//     {
+//         title:"Ipad",
+//         price : 1700,
+//         thumbnail: "https://cdn4.iconfinder.com/data/icons/apple-products-2026/512/iPad_White_Front-512.png",
+//     }
